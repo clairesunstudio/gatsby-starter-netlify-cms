8e29a82572891eae53bd5a13880fd2edfb88dcd3
Minghua Sun <clairesunstudio@gmail.com>
2019-03-31 17:41:53 -0400

    add footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import SocialMedia from '../SocialMedia'
+import { Col } from 'react-bootstrap'
+
+import './index.scss'
+
+class Footer extends React.Component{
+  render = () =>  {
+    const socialIconStyle ={
+      width:30,height:30, margin:3
+    }
+    const socialIconColor = "#fff"
+    return (
+      <footer>
+        <div className="container">
+            <Col sm={6}>
+                <p><strong>© 2017 Claire Sun Studio </strong></p>
+            </Col>
+            <Col sm={6}>
+            <SocialMedia socialIconStyle={socialIconStyle} socialIconColor={socialIconColor}/>
+            </Col>
+        </div>
+      </footer>
+
+    );
+  };
+}
+
+export default Footer
