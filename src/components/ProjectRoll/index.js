4643f4a4002a91050c98048d7de35ceb4100fd62
Minghua Sun <clairesunstudio@gmail.com>
2019-04-03 09:38:02 -0400

    attempt to add project with bootstrap card

---
+++ src/components/ProjectRoll/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql, StaticQuery } from 'gatsby'
+import ProjectCard from '../ProjectCard'
+
+class ProjectRoll extends React.Component {
+  render() {
+    const { data } = this.props
+    const { edges: posts } = data.allMarkdownRemark
+
+    return (
+      <div className="columns is-multiline">
+        {posts &&
+          posts.map(({ node: post }) => (
+            <ProjectCard />
+          ))}
+      </div>
+    )
+  }
+}
+
+ProjectRoll.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+}
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query ProjectRollQuery {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+        ) {
+          edges {
+            node {
+              excerpt(pruneLength: 400)
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                templateKey
+                date(formatString: "MMMM DD, YYYY")
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={(data, count) => <ProjectRoll data={data} count={count} />}
+  />
+)
