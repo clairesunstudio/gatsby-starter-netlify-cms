c8583abcd425457a62d3870db986623303c431c9
Minghua Sun <clairesunstudio@gmail.com>
2019-04-28 22:37:57 -0400

    cards

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap'
 import { Link } from 'gatsby'
+import PreviewCompatibleImage from '../PreviewCompatibleImage'
 
-const ProjectCard = ({ title, text, buttonText }) => {
+const ProjectCard = ({ title, text, buttonText, image }) => {
   return (
       <Card style={{ width: '22rem' }}>
         <Card.Body>
+          { image && <PreviewCompatibleImage imageInfo={image} />}
           <Card.Title>{title}</Card.Title>
           <Card.Text>
             {text}
           </Card.Text>
-          <Button variant="primary">{buttonText}</Button>
+          { buttonText && <Button variant="primary">{buttonText}</Button>}
         </Card.Body>
       </Card>
     )
