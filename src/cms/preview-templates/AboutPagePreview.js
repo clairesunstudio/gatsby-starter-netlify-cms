85ed82749cb32548b4c437df3f976a9dc60f8139
Minghua Sun <clairesunstudio@gmail.com>
2019-11-13 00:38:45 -0500

    fix about preview template

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,12 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/about-page'
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPageTemplate
-    title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
-  />
-)
+const AboutPagePreview = ({ entry, widgetFor, widgetsFor }) => {
+  const info = widgetsFor('info').map((item) => ({
+    icon: item.getIn(['data', 'icon']),
+    href: item.getIn(['data', 'href']),
+    text: item.getIn(['data', 'text'])
+  }))
+
+  return (
+    <AboutPageTemplate
+      title={entry.getIn(['data', 'title'])}
+      content={widgetFor('body')}
+      job={entry.getIn(['data', 'job'])}
+      blurb={entry.getIn(['data', 'blurb'])}
+      info={info}
+      name={entry.getIn(['data', 'name'])}
+      description={entry.getIn(['data', 'description'])}
+      image={entry.getIn(['data', 'image'])}
+    />
+  )
+}
 
 AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
