e5d54133da8a41e9118a57677d99ba7dbb869c8d
Minghua Sun <clairesunstudio@gmail.com>
2019-12-14 14:12:50 -0500

    attempt to use string interpolation in graphql query

--- src/components/Photo/index.js
+++ src/components/Photo/index.js
@@ -1,18 +1,66 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image';
 
-const Photo = ({ rehyped }) => {
-  const props = JSON.parse(rehyped);
-
-  return (
-    <Img
-      fluid={props}
-      style={{
-        maxWidth: props.presentationWidth,
-        margin: '0 auto',
-      }}
-    />
-  );
+const Photo = ({ data, rehyped }) => {
+  console.log(data)
+  console.log(rehyped)
+  return(<div />)
+  // return (
+  //   <Img
+  //     fluid={props}
+  //     style={{
+  //       maxWidth: props.presentationWidth,
+  //       margin: '0 auto',
+  //     }}
+  //   />
+  // );
 };
 
-export default Photo;
+export default ({ rehyped }) => {
+  const data = useStaticQuery(graphql`
+    query MQuery {
+      imageSharp(fixed: {src: {regex: ${`/.*chapter55.jpg/`}}}) {
+        internal {
+          content
+          description
+          ignoreType
+          mediaType
+        }
+        fluid {
+          src
+        }
+        fixed {
+          src
+        }
+      }
+    }
+  `)
+  return (
+    <Photo data={data} rehyped={rehyped} />
+  )
+}
+
+// export default ({ rehyped }) => (
+//   <StaticQuery
+//     query={graphql`{
+//       imageSharp(fixed: {src: {regex: ${`/.*chapter55.jpg/`}}}) {
+//         internal {
+//           content
+//           description
+//           ignoreType
+//           mediaType
+//         }
+//         fluid {
+//           src
+//         }
+//         fixed {
+//           src
+//         }
+//       }
+//     }
+//
+//     `}
+//     render={(data) => <Photo data={data} rehyped={rehyped} />}
+//   />
+// )
