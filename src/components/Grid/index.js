e04426921490ac87c105c0f74b26a414dd38dd37
Minghua Sun <clairesunstudio@gmail.com>
2020-01-01 20:42:59 -0500

    fix css

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -3,9 +3,10 @@ import React from 'react';
 const getGridStyle = (col) => ({
   display: 'grid',
   margin: '2rem 0',
-  'grid-column-gap': '50px',
-  'grid-template-columns': `repeat(${col || 2}, auto)`,
+  gridColumnGap: '50px',
+  gridTemplateColumns: `repeat(${col || 2}, auto)`,
 })
+
 const GridComponent = (props) => (
   <div className='grid' style={getGridStyle(props.col)}>
     {props.children}
